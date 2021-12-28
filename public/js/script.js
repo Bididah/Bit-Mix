b44de41c2ff330d5411398a0ed8c88bbcf6857ad
Bididah <hanfihamza20@gmail.com>
2021-12-29 00:52:56 +0100

    first task

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,23 @@
 // Drum Arrays
-let kicks
-let snares
-let hiHats
-let rideCymbals
+let kicks = new Array[16].fill(false);
+let snares = new Array[16].fill(false);
+let hiHats = new Array[16].fill(false);
+let rideCymbals = new Array[16].fill(false);
 
+const toggleDrum = (arrayName, index) => {
+  arrayName[index] = !arrayName[index];
+};
+
+const clear = (arrayName) => {
+  arrayName.forEach((element) => (element = false));
+};
+
+const invert = (arrayName) => {
+  arrayName.forEach((e) => {
+    if (e) {
+      e = false;
+    } else {
+      e = true;
+    }
+  });
+};
